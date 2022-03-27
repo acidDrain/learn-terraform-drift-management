9e459a6b4e62ae9bb89fb67f130f313823a9db91
James Grow <jmgrow@gmail.com>
2022-03-27 08:06:52 -0500

    Fixed

--- main.tf
+++ main.tf
@@ -79,3 +79,25 @@ resource "aws_security_group" "sg_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "sg_web" {
+  name        = "sg_web"
+  description = "allow 8080"
+}
+
+resource "aws_security_group_rule" "sg_web" {
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
